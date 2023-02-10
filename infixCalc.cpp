ac72cddc6a1f923bf6736c3a0c9aabbff57a2e7e
John Caruthers <john.d.caruthers@gmail.com>
2023-02-09 22:37:45 -0500

    can work with floating point numbers now

--- infixCalc.cpp
+++ infixCalc.cpp
@@ -1,4 +1,4 @@
-/// John Caruthers
+// John Caruthers
 // CS520 Algorithms, Spring 2023
 // Hood College 
 // 07Feb23
@@ -10,7 +10,7 @@
 
 int main() {
     // Create Stacks
-    std::stack<int> value_stack;
+    std::stack<float> value_stack;
     std::stack<char> operand_stack;
 
     // Initialize mathematical expression as a string, get length
@@ -21,20 +21,20 @@ int main() {
 
     // Initialize tempnum for pushing an integer to value_stack
     // tempnumStr is to hold integers of multiple places
-    int tempnum;
+    float tempnum;
     std::string tempnumStr;
 
     // Iterate through math_exp, pushing to approprate stack
     for (int i = 0; i < input_length; i++) {
         // If statement detects if nummber, if so, adds to temp string until hits operand or right parenthesis
-        if (math_exp[i] >= '0' && math_exp[i] <= '9') {
+        if (math_exp[i] >= '0' && math_exp[i] <= '9' || math_exp[i] == '.') {
             tempnumStr.push_back(math_exp[i]);
 
         // First else if detects if an operand, pushes to operand stack if true
-        } else if (math_exp[i] == '+' || math_exp[i] == '-' || math_exp[i] == '*' || math_exp[i] == '/' || math_exp[i] == '%') {
+        } else if (math_exp[i] == '+' || math_exp[i] == '-' || math_exp[i] == '*' || math_exp[i] == '/') {
             // convert temp num string to integer and push to stack (once verified no more numbers)
             if (tempnumStr != "") {
-                tempnum = std::stoi(tempnumStr);
+                tempnum = std::stof(tempnumStr);
                 tempnumStr = "";
                 value_stack.push(tempnum);
             }
@@ -46,17 +46,17 @@ int main() {
         } else if (math_exp[i] == ')') {
             // convert temp num string to integer and push to stack (once verified no more numbers)
             if (tempnumStr != "") {
-                tempnum = std::stoi(tempnumStr);
+                tempnum = std::stof(tempnumStr);
                 tempnumStr = "";
                 value_stack.push(tempnum);
             }
 
             // gets and pops items
-            int value1 = value_stack.top();
+            float value1 = value_stack.top();
             value_stack.pop();
-            char char1 = operand_stack.top();
+            float char1 = operand_stack.top();
             operand_stack.pop();
-            int value2 = value_stack.top();
+            float value2 = value_stack.top();
             value_stack.pop();
 
             // performs calculations
@@ -68,8 +68,6 @@ int main() {
                 value2 *= value1;
             } else if (char1 == '/') {
                 value2 /= value1;
-            } else {
-                value2 %= value1;
             }
 
             // push new value to value_stack
