bee69337475d147a3219e646453c09b505ffc19b
John Caruthers <john.d.caruthers@gmail.com>
2023-02-12 18:42:42 -0500

    created checker function
    checker function consolidates most of the checking into one function, can return multiple errors.

--- infixCalc.cpp
+++ infixCalc.cpp
@@ -8,6 +8,40 @@
 #include <stack>
 #include <string>
 
+bool checker(int numlp, int numrp, int numop) {
+    //counter
+    int counter = 0;
+
+    if (numrp > numlp && numrp > numop) {
+        std::cout << "INPUT ERROR: Please use infix format, verify that parenthesis are correctly oriented" << std::endl;
+        counter++;
+    }
+
+    // check for presence of operands
+    if (numop == 0) {
+        std::cout << "INPUT ERROR: No operands detected, please verify expression is correctly inputted in infix format." << std::endl;
+        counter++;
+    }
+
+    if (numop > numrp) {
+        std::cout << "INPUT ERROR: There are either too many operands or not enough ')', please verify expression is correctly inputted in infix format." << std::endl;
+        counter++;
+    }
+
+    // perform final check
+    if (numrp != numlp || numrp != numop || numrp == 0 || numlp == 0) {
+        std::cout << "INPUT ERROR: You are either missing or have extra parenthesis, please verify expression is correctly inputted in infix format." << std::endl;
+        counter++;
+    }
+
+    if (counter > 0) {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
 int main() {
     // Create Stacks
     std::stack<float> value_stack;
@@ -19,6 +53,11 @@ int main() {
     std::cin >> math_exp;
     int input_length = math_exp.size();
 
+    //Initialize counter integers
+    int lpar = 0;
+    int rpar = 0;
+    int oper = 0;
+
     // Initialize tempnum for pushing an integer to value_stack
     // tempnumStr is to hold integers of multiple places
     float tempnum;
@@ -26,12 +65,16 @@ int main() {
 
     // Iterate through math_exp, pushing to approprate stack
     for (int i = 0; i < input_length; i++) {
+
         // If statement detects if nummber, if so, adds to temp string until hits operand or right parenthesis
         if (math_exp[i] >= '0' && math_exp[i] <= '9' || math_exp[i] == '.') {
             tempnumStr.push_back(math_exp[i]);
 
         // First else if detects if an operand, pushes to operand stack if true
         } else if (math_exp[i] == '+' || math_exp[i] == '-' || math_exp[i] == '*' || math_exp[i] == '/') {
+            // Add to operant counter
+            oper++;
+
             // convert temp num string to integer and push to stack (once verified no more numbers)
             if (tempnumStr != "") {
                 tempnum = std::stof(tempnumStr);
@@ -44,12 +87,29 @@ int main() {
 
         // Last conditional detects if a right parenthesis, pops items, performs calculations, pushes result to value stack
         } else if (math_exp[i] == ')') {
+            // add to rpar
+            rpar++;
+
+            // checker(lpar, rpar, oper);
+
             // convert temp num string to integer and push to stack (once verified no more numbers)
             if (tempnumStr != "") {
                 tempnum = std::stof(tempnumStr);
                 tempnumStr = "";
                 value_stack.push(tempnum);
             }
+
+            // detects if there areat least two values
+            if (value_stack.size() < 2) {
+                std::cout << "INPUT ERROR: A ')' has been used with no values, or only one value, please verify expression is correct." << std::endl;
+                return 3;
+            }
+
+            if (operand_stack.size() == 0) {
+                std::cout << "INPUT ERROR: A ')' has been used with no operand, please verify expression is correct." << std::endl;
+                return 3;
+            }
+
 
             // gets and pops items
             float value1 = value_stack.top();
@@ -72,10 +132,24 @@ int main() {
 
             // push new value to value_stack
             value_stack.push(value2);
+
+        } else if (math_exp[i] == '(') {
+            lpar++;
+
+        // detects illegal characters
+        } else {
+            std::cout << "INPUT ERROR: '" << math_exp[i] << "'" << " is not a valid character, please verify expression is correct." << std::endl;
+            return 3;
         }
     }
 
-    // Once iterated through entire math_exp, print out result
-    std::cout << "Result of expression: " << value_stack.top() << std::endl;
-    return 0;
+    if (checker(lpar, rpar, oper)) {
+        // Once iterated through entire math_exp, print out result
+        std::cout << "Result of expression: " << value_stack.top() << std::endl;
+        return 0;
+    } else {
+        return 3;
+    }
+
+    
 }
