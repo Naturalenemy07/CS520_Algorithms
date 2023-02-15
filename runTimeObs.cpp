0df7a2a56d01c41ac86fc44ef45e33985cdc042f
John Caruthers <john.d.caruthers@gmail.com>
2023-02-14 23:46:06 -0500

    Create runTimeObs.cpp

---
+++ runTimeObs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <chrono>
+
+//set variables (n) number of ints, (range), and (m) number of ints to search within n
+
+// generate (n) integers and save to an array (n_ints)
+
+// sort list of (n) integers
+
+// generate list of (m) integers to be searched and save to seperate array (m_ints)
+
+// create array (linear_search_times, and binary_search_times) to store all search times, initialize (lin_num_found, and bin_num_found) to store numebr of integers found
+
+// for each number in M_ints, search using linear search method
+// // keep track of time to perform search using <chrono>, save the time (in milliseconds) to (search_times)
+
+// for each number in M_ints, search using binary search method
+// // keep track of time to perform search using <chrono>, save the time (in milliseconds) to (search_times)
+
+// once completed, calculate average time to seach
+
+// print out (n), (m), number found and average time spent searching for each ex: 
+// (n): 100, (m): 50, (num found): #, (linear avg time): # secs, (binary avg time): # secs
