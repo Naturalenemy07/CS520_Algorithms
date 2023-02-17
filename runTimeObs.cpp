0b6ed5a020f5e0edaf91ed820b6baac08cdc6307
John Caruthers <john.d.caruthers@gmail.com>
2023-02-16 22:37:49 -0500

    arrays created, random nums generated

--- runTimeObs.cpp
+++ runTimeObs.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
+#include <array>
 #include <chrono>
+#include <cstdlib>
+#include <algorithm>
 
 
 int main() {
-    //set variables (data_size) is number of ints, (range), and (m_ints) sample size
+    // Set (data_size) is number of ints in dataset, (range) is the range of that dataset, and (m_ints) is the sample size
     int data_size;
     int d_range;
     int m_ints;
 
+    // Get user input on dataset size and sample size
     std::cout << "Enter data size: ";
     std::cin >> data_size;
     std::cout <<"Enter sample size: ";
     std::cin >> m_ints;
     d_range = 10*data_size;
 
-    // generate (n) integers and save to an array (n_ints)
-    
+    // Create empty static array of size (data_size) to hold entire dataset, 
+    // and empty static array to hold integers that will be searched for in the dataset (sample_array)
+    int data_array[data_size];
+    int sample_array[m_ints];
+
+    // create array (linear_search_times, and binary_search_times) to store all search times, initialize (lin_num_found, and bin_num_found)
+    int linear_search_times[data_size];
+    int binary_search_times[data_size];
+    int lin_num_found;
+    int bin_num_found;
+
+    // randomly generate (data_size) integers and save to array (data_array)
+    // use of std::rand() is fine for this not serious purpose, and the range is 10x the data_size, to minimize prob of repeats
+    for (int dummy_n = 0; dummy_n < data_size; dummy_n++) {
+        data_array[dummy_n] = (std::rand() % d_range);
+    }
 
     // sort list of (n) integers
+    std::sort(data_array,data_array + data_size);
 
     // generate list of (m) integers to be searched and save to seperate array (m_ints)
+    for (int dummy_m = 0; dummy_m < data_size; dummy_m++) {
+        sample_array[dummy_m] = (std::rand() % d_range);
+    }
 
     // create array (linear_search_times, and binary_search_times) to store all search times, initialize (lin_num_found, and bin_num_found) to store numebr of integers found
 
