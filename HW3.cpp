69fa7ffc720e55006883da3faa9e18ee35328db0
John Caruthers <john.d.caruthers@gmail.com>
2023-02-28 22:09:40 -0500

    confirmSorted added

--- HW3.cpp
+++ HW3.cpp
@@ -3,8 +3,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <chrono>
 
-bool confirmSorted() {
+bool confirmSorted(std::vector<int>& array_input, int len_input) {
+    int sort_counter = 0;
+    int int_counter = 0;
+    while (sort_counter == 0 && int_counter < len_input-1) {
+        if (array_input[int_counter] > array_input[int_counter + 1]) {
+            sort_counter++;
+            return false;
+        }
+        int_counter++;
+    }
     return true;
 }
 
@@ -30,15 +40,26 @@ int main () {
     else std::cout << "Unable to open file";
 
     len = data_array.size();
+    std::cout << "Reading data from: " << input_text << std::endl;
 
     // Ask what method want to sort
     std::string method;
     std::cout << "Choose method of sorting (S)Selection, (I)Insertion, (B)Bubble, (M)Merge, or (Q)Quick Sort: ";
     std::cin >> method;
 
+    // See if sorted
+    if (confirmSorted(data_array, len)) {
+        std::cout << "Confirmed Sorted" <<std::endl;
+    } else {
+        std::cout << "Confirmed NOT Sorted" << std::endl;
+    }
+
     ///////////////
     /// Methods ///
     ///////////////
+
+    // Start time.
+    auto start = std::chrono::high_resolution_clock::now();
 
     // Selection Sort
     if (method == "S") {
@@ -46,7 +67,6 @@ int main () {
         // perform selection sort
         int min = 0;
         for (int i = 0; i < len; i++) {
-            std::cout << i << std::endl;
             min = i;
             int j = i;
             while (j < len-1) {
@@ -61,5 +81,44 @@ int main () {
                 data_array[min] = temp;
             }
         }
+    }
+
+    // Insertion Sort
+    if (method == "I") {
+        for (int dum_i = 1; dum_i < len; dum_i++) {
+        // compare array[dum_i] to item on left
+            int temp = 0;
+            int count = dum_i;
+            // repeat while array[dum_i] is less than item to its left
+            while (data_array[count] < data_array[count -1] && count > 0) {
+                temp = data_array[count];
+                data_array[count] = data_array[count-1];
+                data_array[count-1] = temp;
+                count--;
+            }
+        }
+    }
+
+    // Quicksort
+    if (method == "Q") {
+        
+    }
+
+    // Mergesort
+    if (method == "M") {
+        
+    }
+
+    // End time, get search time.
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> duration = end - start;
+    std::cout << "It took " << (duration.count())*1000 << " milliseconds." << std::endl;
+
+
+    // See if sorted
+    if (confirmSorted(data_array, len)) {
+        std::cout << "Confirmed Sorted" <<std::endl;
+    } else {
+        std::cout << "Confirmed NOT Sorted" << std::endl;
     }
 }
