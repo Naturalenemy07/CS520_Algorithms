2ae2cb96a0f4ff0d762e324d33940fa52f9a1599
John Caruthers <john.d.caruthers@gmail.com>
2023-03-01 07:28:48 -0500

    created seperate functions for sorting algorithms

--- HW3.cpp
+++ HW3.cpp
@@ -1,4 +1,3 @@
-// reading a text file
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,6 +15,43 @@ bool confirmSorted(std::vector<int>& array_input, int len_input) {
         int_counter++;
     }
     return true;
+}
+
+void selectionSort(std::vector<int>& array_input, int len_input) {
+    std::cout << "Initiating Selection Sorting" << std::endl;
+    // perform selection sort
+    int min = 0;
+    for (int i = 0; i < len_input; i++) {
+        min = i;
+        int j = i;
+        while (j < len_input-1) {
+            j++;
+            if (array_input[j] < array_input[min]) {
+                min = j;
+            } 
+        }
+        if (min != i) {
+            int temp = array_input[i];
+            array_input[i] = array_input[min];
+            array_input[min] = temp;
+        }
+    }
+    return;
+}
+
+void insertionSort(std::vector<int>& array_input, int len_input) {
+    for (int i = 1; i < len_input; i++) {
+    // compare array[dum_i] to item on left
+        int temp = 0;
+        int count = i;
+        // repeat while array[dum_i] is less than item to its left
+        while (array_input[count] < array_input[count -1] && count > 0) {
+            temp = array_input[count];
+            array_input[count] = array_input[count-1];
+            array_input[count-1] = temp;
+            count--;
+        }
+    }
 }
 
 int main () {
@@ -44,8 +80,15 @@ int main () {
 
     // Ask what method want to sort
     std::string method;
-    std::cout << "Choose method of sorting (S)Selection, (I)Insertion, (B)Bubble, (M)Merge, or (Q)Quick Sort: ";
+    std::cout << "Choose method of sorting (S)Selection, (I)Insertion, (M)Merge, or (Q)Quick Sort: ";
     std::cin >> method;
+    std::cout << '\n';
+
+    // Check validity of selection
+    if (method != "S" || method != "I" || method != "M" || method != "Q" || method != "s" || method != "i" || method != "m" || method != "q") {
+        std::cout << "'" << method << "'" << " is not a proper sorting algorithm, please run program again, and select proper sorting algorithm." << std::endl;
+        return 1;
+    }
 
     // See if sorted
     if (confirmSorted(data_array, len)) {
@@ -54,60 +97,19 @@ int main () {
         std::cout << "Confirmed NOT Sorted" << std::endl;
     }
 
-    ///////////////
-    /// Methods ///
-    ///////////////
-
     // Start time.
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Selection Sort
-    if (method == "S") {
-        std::cout << "Initiating Selection Sorting" << std::endl;
-        // perform selection sort
-        int min = 0;
-        for (int i = 0; i < len; i++) {
-            min = i;
-            int j = i;
-            while (j < len-1) {
-                j++;
-                if (data_array[j] < data_array[min]) {
-                    min = j;
-                } 
-            }
-            if (min != i) {
-                int temp = data_array[i];
-                data_array[i] = data_array[min];
-                data_array[min] = temp;
-            }
-        }
-    }
-
-    // Insertion Sort
-    if (method == "I") {
-        for (int dum_i = 1; dum_i < len; dum_i++) {
-        // compare array[dum_i] to item on left
-            int temp = 0;
-            int count = dum_i;
-            // repeat while array[dum_i] is less than item to its left
-            while (data_array[count] < data_array[count -1] && count > 0) {
-                temp = data_array[count];
-                data_array[count] = data_array[count-1];
-                data_array[count-1] = temp;
-                count--;
-            }
-        }
-    }
-
-    // Quicksort
-    if (method == "Q") {
-        
-    }
-
-    // Mergesort
-    if (method == "M") {
-        
-    }
+    // Sorting algorithm selection
+    if (method == "S" || method == "s") {
+        selectionSort(data_array, len);
+    } else if (method == "I") {
+        insertionSort(data_array, len);
+    } else if (method == "Q") {
+        std::cout << "in progress";
+    } else {
+        std::cout << "in progress";
+    } 
 
     // End time, get search time.
     auto end = std::chrono::high_resolution_clock::now();
