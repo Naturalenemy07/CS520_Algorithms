ff49c507ad451a5962aec63fa25c4f9fc4a58b20
John Caruthers <john.d.caruthers@gmail.com>
2023-03-05 22:43:45 -0500

    implementing median of three
    sorting and partition still dont work

--- learning/quicksort.cpp
+++ learning/quicksort.cpp
@@ -3,10 +3,38 @@
 #include<iostream>
 #include<vector>
 
+void median_of_three(std::vector<int>& array, int len) {
+   int first = array[0];
+   int middle = array[len/2];
+   int last = array[len-1];
+
+   std::cout << first << " " << middle << " " << last << std::endl;
+
+   int temp_array[3];
+
+   if (first < middle) {
+      temp_array[0] = first;
+      temp_array[1] = middle;
+   } else {
+      temp_array[0] = middle;
+      temp_array[1] = first;
+   }
+   if (middle < last) {
+      temp_array[2] = last;
+   } else {
+      temp_array[1] = last;
+      temp_array[2] = middle;
+   }
+   if (last < first) {
+      temp_array[0] = last;
+      temp_array[1] = first;
+   }
+}
+
 void print_array(std::vector<int>& array, int len) {
    // print input array
    std::cout << "[";
-   for (int i = 0; i < len; i++) {
+   for (int i = 0; i <= len; i++) {
       std::cout << array[i] << " ";
    }
    std::cout << "]" << std::endl;
@@ -18,7 +46,7 @@ void swap(std::vector<int>& array, int index_a, int index_b) {
    array[index_b] = temp;
 }
 
-int partition(std::vector<int>& array, int begin, int end) {
+int partition(std::vector<int>& array, int begin, int end, int len) {
    // create scanners
    int front_scan = begin;
    int rear_scan = end;
@@ -26,10 +54,11 @@ int partition(std::vector<int>& array, int begin, int end) {
    // pivot value is the first digit
    //need to select the median of three as the pivot value
    int pivot_value = array[begin];
+   median_of_three(array, len);
 
    // partitioning
    while (true) {
-      while (array[front_scan] < pivot_value) {
+      while (array[front_scan] <= pivot_value) {
          front_scan++;
          if (front_scan == end) {
             break;
@@ -41,18 +70,21 @@ int partition(std::vector<int>& array, int begin, int end) {
             break;
          }
       }
+      break;
     }
-    swap(array, front_scan, rear_scan);
-    return rear_scan;
+    if (front_scan < rear_scan) {
+      swap(array, front_scan, rear_scan);
+      return rear_scan;
+    }
 }
 
-void sort(std::vector<int>& array, int low, int high) {
+void qsort(std::vector<int>& array, int low, int high, int len) {
    if (high <= low) {
       return;
    }
-   int pivoted = partition(array, low, high); 
-   sort(array, low, pivoted-1);
-   sort(array, pivoted+1, high);
+   int pivoted = partition(array, low, high, len);
+   qsort(array, low, pivoted-1, len);
+   qsort(array, pivoted+1, high, len);
 }
 
 int main() {
@@ -69,11 +101,11 @@ int main() {
    }
 
    // print current array
-   print_array(array_to_sort, len);
+   print_array(array_to_sort, len-1);
 
    // perform quick sort //
-   sort(array_to_sort, 0, len);
+   qsort(array_to_sort, 0, len-1, len);
 
    // print current array
-   print_array(array_to_sort, len);
+   print_array(array_to_sort, len-1);
 }
